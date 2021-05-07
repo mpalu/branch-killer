2eafb4237c9065a3a2f0d5943adf097b84eb67e2
Matheus Palu <matheus_palu@trimble.com>
2021-05-07 19:36:08 -0300

    updating script

--- branch-killer.sh
+++ branch-killer.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-GIT_FOLDER=("$HOME/workspace/bitbucket" "$HOME/workspace/github")
+PROJECTS_FOLDER="$HOME/WorkSpace/"
 
-for parent_directory in ${GIT_FOLDER[*]} ; do
-  cd "$parent_directory" || exit
+cd "$PROJECTS_FOLDER" || exit
 
-  for project in */ ; do
-    cd "$project" || exit
+for project in */ ; do
+  cd "$project" || exit
 
-    DELETE_MESSAGE="- Deleting local branches from $project"
-    echo "${DELETE_MESSAGE%?}"
-    git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D > /dev/null 2>&1
-    
-    cd ..
-  done
+  DELETE_MESSAGE="- Deleting local branches from $project"
+  echo "${DELETE_MESSAGE%?}"
+  git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D > /dev/null 2>&1
   
   cd ..
 done
+  
